7072c597bf17d9a7a42508169e1fd8d2fd055815
pabox <pabox@zedat.fu-berlin.de>
2024-09-15 14:15:20 +0200

    Add needed information for direction and naming

--- src/main/java/io/github/_4drian3d/simplejumppads/configuration/Configuration.java
+++ src/main/java/io/github/_4drian3d/simplejumppads/configuration/Configuration.java
@@ -2,8 +2,11 @@ package io.github._4drian3d.simplejumppads.configuration;
 
 import net.kyori.adventure.key.Key;
 import net.kyori.adventure.sound.Sound;
+
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
+import org.bukkit.World;
 import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 import org.spongepowered.configurate.objectmapping.meta.Comment;
 
@@ -19,23 +22,62 @@ public class Configuration {
 
     @ConfigSerializable
     public static class Section {
+        @Comment("Sets the name of this JumpPad")
+        private String name = "default";
+
         @Comment("Sets the material of this JumpPad")
         private Material material = Material.HEAVY_WEIGHTED_PRESSURE_PLATE;
+
+        @Comment("Sets the dimension of this PumpPad")
+        private World world = null;
+
+        @Comment("Sets the X location of this PumpPad")
+        private double locationX = Double.NaN;
+
+        @Comment("Sets the Y location of this JumpPad")
+        private double locationY = Double.NaN;
+
+        @Comment("Sets the Z location of this JumpPad")
+        private double locationZ = Double.NaN;
+
+        @Comment("Sets the player rotation of this PumpPad")
+        private double fixRotation = Double.NaN;
+
+        @Comment("Sets the player launch angle of this JumpPad")
+        private double fixAngle = Double.NaN;
+
         @Comment("Sets the horizontal multiplier of this PumpPad")
         private double multiplyX = 1.0;
+
         @Comment("Sets the vertical multiplier of this JumpPad")
         private double multiplyY = 1.0;
+
         @Comment("Enable sound when using this JumpPad")
         private boolean enableSound = true;
+
         @Comment("Enables particles to be displayed when using this JumpPad")
         private boolean enableParticle = false;
+
         @Comment("Sets what sound will be played when using this JumpPad if sounds are enabled")
         private Sound sound = Sound.sound(Key.key("entity.experience_orb.pickup"), Sound.Source.PLAYER, 1, 1);
+
         @Comment("Sets which particle will be displayed when using this JumpPad if particles are enabled")
         private Particle particle = Particle.CLOUD;
 
         public Material getMaterial() {
             return material;
+        }
+
+        public Location getLocation() {
+            return new Location(world, locationX, locationY, locationZ);
+        }
+
+        public double getFixRotation() {
+            return fixRotation;
+        }
+
+        public double getFixAngle() {
+            return fixAngle;
         }
 
         public double getMultiplyX() {
